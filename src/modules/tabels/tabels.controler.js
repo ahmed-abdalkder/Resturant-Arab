27104fe1d7fe70835ac6ccae44ba6e933764dbd5
Ahmed Abdulkadir <aabdelkder20@gmail.com>
2025-07-15 15:22:53 +0200

    auth

--- src/modules/tabels/tabels.controler.js
+++ src/modules/tabels/tabels.controler.js
@@ -4,18 +4,66 @@ import tableModel from "../../../db/models/table.model.js";
 import { asyncHandeler } from "../../utils/asyncHandeler.js";
 import { AppError } from "../../utils/classAppError.js";
 
+function toArabicNumbers(input) {
+  if (input === undefined || input === null) return "٠";  
+  return input.toString().replace(/\d/g, (d) => '٠١٢٣٤٥٦٧٨٩'[parseInt(d)]);
+}
+
+function toEnglishNumbers(str) {
+  return str
+    .toString()
+    .replace(/[٠١٢٣٤٥٦٧٨٩]/g, (d) => '٠١٢٣٤٥٦٧٨٩'.indexOf(d));
+}
+
+// export const createTable = asyncHandeler(async (req, res, next) => {
+//   const { number, capacity, branchId } = req.body;
+
+//   const exist = await tableModel.findOne({ number, branchId });
+//   if (exist) {
+//     return next(new AppError("Table already exists with this number"));
+//   }
+
+//   const table = new tableModel({ number, capacity, branchId });
+//   await table.save();
+
+//   return res.status(201).json({ msg: "Table", table });
+// });
+
 export const createTable = asyncHandeler(async (req, res, next) => {
   const { number, capacity, branchId } = req.body;
 
-  const exist = await tableModel.findOne({ number, branchId });
+  
+  const parsedNumber = Number(toEnglishNumbers(number));
+  const parsedCapacity = Number(toEnglishNumbers(capacity));
+
+  if (isNaN(parsedNumber) || isNaN(parsedCapacity)) {
+    return next(new AppError("Number and capacity must be valid numbers"));
+  }
+
+  const exist = await tableModel.findOne({ number: parsedNumber, branchId });
   if (exist) {
     return next(new AppError("Table already exists with this number"));
   }
 
-  const table = new tableModel({ number, capacity, branchId });
+  const table = new tableModel({
+    number: parsedNumber,
+    capacity: parsedCapacity,
+    branchId
+  });
+
   await table.save();
 
-  return res.status(201).json({ msg: "Table", table });
+  
+  const arabicTable = {
+    ...table.toObject(),
+    number: toArabicNumbers(table.number),
+    capacity: toArabicNumbers(table.capacity),
+  };
+
+  return res.status(201).json({
+    msg: "تم إنشاء الطاولة بنجاح",
+    table: arabicTable
+  });
 });
 
 export const getTables = asyncHandeler(async (req, res) => {
@@ -27,9 +75,31 @@ export const getTables = asyncHandeler(async (req, res) => {
   res.status(200).json({ msg: "All tables", tables });
 });
 
+// export const updateTable = asyncHandeler(async (req, res, next) => {
+//   const { id } = req.params;
+//   const { number, capacity, branchId } = req.body;
+
+//   const table = await tableModel.findById(id);
+//   if (!table) {
+//     return next(new AppError("Table not found"));
+//   }
+
+//   const existingReservation = await reservationModel.findOne({ tableId: id });
+//   if (existingReservation) {
+//     return next(new AppError("Cannot update: Table has active reservations"));
+//   }
+//   table.number = number || table.number;
+//   table.capacity = capacity || table.capacity;
+//   table.branchId = branchId || table.branchId;
+
+//   await table.save();
+
+//   res.status(200).json({ msg: "Table", table });
+// });
+
 export const updateTable = asyncHandeler(async (req, res, next) => {
   const { id } = req.params;
-  const { number, capacity, branchId } = req.body;
+  let { number, capacity, branchId } = req.body;
 
   const table = await tableModel.findById(id);
   if (!table) {
@@ -40,13 +110,38 @@ export const updateTable = asyncHandeler(async (req, res, next) => {
   if (existingReservation) {
     return next(new AppError("Cannot update: Table has active reservations"));
   }
-  table.number = number || table.number;
-  table.capacity = capacity || table.capacity;
-  table.branchId = branchId || table.branchId;
+
+  // تحويل الأرقام من عربية إلى إنجليزية
+  if (number !== undefined) {
+    const parsedNumber = Number(toEnglishNumbers(number));
+    if (isNaN(parsedNumber)) {
+      return next(new AppError("Number must be a valid number"));
+    }
+    table.number = parsedNumber;
+  }
+
+  if (capacity !== undefined) {
+    const parsedCapacity = Number(toEnglishNumbers(capacity));
+    if (isNaN(parsedCapacity)) {
+      return next(new AppError("Capacity must be a valid number"));
+    }
+    table.capacity = parsedCapacity;
+  }
+
+  if (branchId !== undefined) {
+    table.branchId = branchId;
+  }
 
   await table.save();
 
-  res.status(200).json({ msg: "Table", table });
+  // تجهيز البيانات بالأرقام العربية للعرض
+  const arabicTable = {
+    ...table.toObject(),
+    number: toArabicNumbers(table.number),
+    capacity: toArabicNumbers(table.capacity),
+  };
+
+  res.status(200).json({ msg: "تم تعديل الطاولة بنجاح", table: arabicTable });
 });
 
 export const deleteTable = asyncHandeler(async (req, res, next) => {
